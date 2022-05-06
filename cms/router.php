0d75c1a1e90aa1ba47a590c9c1ed1836404f9a6e
RodrigoBatis <rodrigofreirebatista@gmail.com>
2022-05-06 13:55:20 -0300

    ..

--- cms/router.php
+++ cms/router.php
@@ -130,6 +130,9 @@
                              </script>");
                      }
             }
+            break;
+
+            case "USUARIOS":
         }
     }    
 
--- cms/usuarios.php
+++ cms/usuarios.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css-cms/reset.css">
     <link rel="stylesheet" href="./css-cms/header.css">
+    <link rel="stylesheet" href="./css-cms/sesaoEscolha.css">
+    <link rel="stylesheet" href="./css-cms/sesaoUsuarios.css">
     <title>Gerencionamento-Usuarios</title>
 </head>
 <body>
@@ -21,5 +23,91 @@
             <span id="txtLogo">Good Coffee</span>
         </div>
     </header>
+    <main>
+    <section class="sesaoEscolha">
+            <div class="container-escolha">
+                <a href="">
+                    <img src="img-cms/produtos.png" alt="admProdutos">
+                    <span>Adm. de Produtos</span>
+                </a>
+            </div>
+            <div class="container-escolha">
+                <a href="categorias.php">
+                    <img src="img-cms/categorias.png" alt="categorias">
+                    <span>Adm. de Categorias</span>
+                </a>
+            </div>
+
+            <div class="container-escolha">
+                <a href="contatos.php">
+                    <img src="img-cms/contato.png" alt="contatos">
+                    <span>Contatos</span>
+                </a>
+            </div>
+
+            <div class="container-escolha">
+                <a href="dashboard.php">
+                    <img src="img-cms/usuarios.png" alt="usuarios">
+                    <span>Usuarios</span>
+                </a>
+            </div>
+
+            <div class="container-escolha">
+                <p>Bem Vindo <span>-- Rodrigo --</span></p>
+                <a href="">
+                    <img src="img-cms/sair.png" alt="sair">
+                    <span>Logout</span>
+                </a>
+            </div>
+
+        </section>
+
+            <h1 class="tituloSesao">Usuarios</h1>
+
+            <div class="cadastroUsuarios">
+                <div class="tituloCadastro">
+                    <h1> Cadastro de Usuarios </h1>
+                </div>
+                <div class="cadastroInformacoes">
+                   <form action="" name="frmCadastro" method="post">
+                   <div class="campos">
+                        <div class="cadastroInformacoesPessoais">
+                            <label>Usuario:</label>
+                        </div>
+
+                        <div class="cadastroEntradaDeDados">
+                            <input type="text" name="txtNome" value="" placeholder="Digite seu nome" maxlength="100">
+                        </div>
+                   </div>
+                   <div class="campos">
+                        <div class="cadastroInformacoesPessoais">
+                            <label>Senha:</label>
+                        </div>
+
+                        <div class="cadastroEntradaDeDados">
+                            <input type="text" name="txtNome" value="" placeholder="Digite seu nome" maxlength="100">
+                        </div>
+                   </div>
+                   <div class="campos">
+                        <div class="cadastroInformacoesPessoais">
+                            <label>Confirme sua Senha:</label>
+                        </div>
+
+                        <div class="cadastroEntradaDeDados">
+                            <input type="text" name="txtNome" value="" placeholder="Digite seu nome" maxlength="100">
+                        </div>
+                   </div>
+                   <div class="enviar">
+                        <div class="enviar">
+                            <input type="submit" name="btnEnviar" value="Salvar">
+                        </div>
+                    </div>
+                   </form>
+
+                </div>
+
+            </div>
+
+    </main>
 </body>
 </html>
